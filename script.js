7d22bd4852053c16e1ab3bf9a7c158c116753c99
mmboyce <mathieumimmsboyce@gmail.com>
2020-01-05 22:40:29 -0500

    Add render function

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
+const booksContainer = document.querySelector("#books")
+const addBookButton = document.querySelector("#add-book")
+
 function Book(title, author, pages, hasBeenRead){
     this.title = title
     this.author = author
     this.pages = pages
     this.hasBeenRead = hasBeenRead
-}
-
-Book.prototype.info = function(){
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasBeenRead ? 'not read yet' : 'has been read'}`
 }
 
 const book1 = new Book("IT", "Stephen King", 1388, false)
@@ -17,3 +16,52 @@ let myLibrary = [book1, book2]
 function addBookToLibrary(book){
     myLibrary.push(book)
 }
+
+function createBookElement(book){
+    let list = document.createElement("ul")
+
+    for(attribute in book){
+        let item = document.createElement("li")
+        item.className = attribute
+        item.innerText = book[attribute]
+
+        if(attribute == "pages"){
+            item.innerText += " pages"
+        }
+
+        if(attribute == "hasBeenRead"){
+            item.innerText = `${book[attribute] ? 'Has been read.' : 'Has not been read yet.'}`
+        }
+
+        list.appendChild(item)
+    }
+
+    list.className = "book"
+
+    return list
+}
+
+function removeBooksFromWindow(){
+    let books = booksContainer.querySelectorAll(".book")
+
+    books.forEach(book => {
+        booksContainer.removeChild(book)
+    })
+}
+
+function renderLibrary(){
+    // we call this so that we can rerender all the books without readding
+    // the ones already present
+    removeBooksFromWindow()
+
+    myLibrary.forEach(book => {
+        booksContainer.appendChild(createBookElement(book))
+    })
+}
+
+
+renderLibrary()
+
+addBookButton.addEventListener("click", () => {
+    console.log("clicked")
+})
